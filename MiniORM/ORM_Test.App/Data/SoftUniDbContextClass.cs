c3de510334421aaccc7f3dd98206ca7829a33ce7
Daniel G <dani.gwenski@gmail.com>
2019-06-26 08:13:48 +0300

    Added ORM test

---
+++ MiniORM/ORM_Test.App/Data/SoftUniDbContextClass.cs
@@ -0,0 +1,22 @@
+﻿using MiniORM.App.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniORM.App.Data
+{
+    public class SoftUniDbContextClass : DbContext
+    {
+        public SoftUniDbContextClass(string connectionString) : base(connectionString)
+        {
+        }
+
+        public DbSet<Department> Department { get; }
+
+        public DbSet<Employee> Employee { get; }
+
+        public DbSet<Project> Project { get; }
+
+        public DbSet<EmployeeProject> EmployeeProjects { get; }
+    }
+}
---
+++ MiniORM/ORM_Test.App/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using MiniORM.App.Data;
+using System;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    class StartUp
+    {
+        static void Main()
+        {
+
+            var context = new SoftUniDbContextClass(connection);
+
+            context.Employee.Add(new Data.Entities.Employee
+            {
+                FirstName = "John",
+                LastName = "Snow",
+                DepartmentId = context.Department.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employee.Last();
+            employee.FirstName = "Ben";
+
+            context.SaveChanges();
+        }
+    }
+}
+
+
