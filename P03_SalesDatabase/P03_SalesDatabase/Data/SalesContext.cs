1921de1bb4c28618122416084893a4dd70c8a0f0
Daniel G <dani.gwenski@gmail.com>
2019-07-01 08:43:06 +0300

    commit SalesDB

---
+++ P03_SalesDatabase/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Connection.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingCustomer(modelBuilder);
+
+            OnModelCreatingProduct(modelBuilder);
+
+            OnModelCreatingSale(modelBuilder);
+
+            OnModelCreatingStore(modelBuilder);
+        }
+
+        private void OnModelCreatingStore(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Store>()
+                 .HasKey(s => s.StoreId);
+
+            modelBuilder.Entity<Store>()
+                 .Property(s => s.Name)
+                 .HasMaxLength(80)
+                 .IsUnicode();
+
+            modelBuilder.Entity<Store>()
+                 .HasMany(st => st.Sales)
+                 .WithOne(s => s.Store);
+        }
+
+        private void OnModelCreatingSale(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sale>()
+                .HasKey(s => s.SaleId);
+
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Product)
+                .WithMany(p => p.Sales);
+
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Customer)
+                .WithMany(c => c.Sales);
+
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Store)
+                .WithMany(s => s.Sales);
+        }
+
+        private void OnModelCreatingProduct(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasKey(p => p.ProductId);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsRequired()
+                .IsUnicode();
+
+            modelBuilder.Entity<Product>()
+                .HasMany(p => p.Sales)
+                .WithOne(s => s.Product);
+        }
+
+        private void OnModelCreatingCustomer(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+               .HasKey(c => c.CustomerId);
+
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsUnicode()
+                .IsRequired();
+
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Email)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Customer>()
+                .HasMany(c => c.Sales)
+                .WithOne(s => s.Customer);
+        }
+    }
+}
