ed11f52e050d408261745ea7ea1ce8d6679e9dad
Daniel G <dani.gwenski@gmail.com>
2019-07-04 08:14:02 +0300

    Added two commands
    GetBooksByPriceCommand
    GetGoldenBooksCommand

---
+++ BookShopSystem/BookShop/Core/Commands/GetBooksByPriceCommand.cs
@@ -0,0 +1,37 @@
+﻿using BookShop.Models;
+using BookShop_GoldenBooks_Added.Core.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace BookShop_GetBookByPrice.Core.Commands
+{
+    public class GetBooksByPriceCommand : ICommand
+    {
+        private readonly BookShopContext context;
+
+        public GetBooksByPriceCommand(BookShopContext context)
+        {
+            this.context = context;
+        }
+
+        public string Execute(string[] args)
+        {
+            return GetBooksByPrice(context);
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.Price > 40)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var book in books.OrderByDescending(b => b.Price))
+            {
+                sb.AppendLine($"{book.Title} - {book.Price}");
+            }
+            return sb.ToString();
+        }
+    }
+}
