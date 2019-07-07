3a6d52c9d01fe54ce35e78cf82f82c5826165786
Daniel G <dani.gwenski@gmail.com>
2019-07-07 08:26:18 +0300

    Create BookSearchByAuthorCommand.cs

---
+++ BookShopSystem/BookShop/Core/Commands/BookSearchByAuthorCommand.cs
@@ -0,0 +1,50 @@
+﻿using BookShop.Data;
+using BookShop_GoldenBooks_Added.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookShop.Core.Commands
+{
+    public class BookSearchByAuthorCommand : ICommand
+    {
+        private readonly BookShopContext context;
+
+        public BookSearchByAuthorCommand(BookShopContext context)
+        {
+            this.context = context;
+        }
+
+        public string Execute(string[] args)
+        {
+            var input = args[0].ToLower();
+
+            return GetBooksByAuthor(context, input);
+        }
+
+        public static string GetBooksByAuthor(BookShopContext context, string input)
+        {
+            var books = context.Books
+                .Where(book => book.Author.LastName.StartsWith(input))
+                .Select(x => new
+                {
+                    x.BookId,
+                    x.Title,
+                    x.Author.FirstName,
+                    x.Author.LastName
+                })
+                .OrderBy(book => book.BookId)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} ({book.FirstName} {book.LastName})");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
