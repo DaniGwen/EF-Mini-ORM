2de250947e4ab692682257f6b6c7c224b4d69ac0
Daniel G <dani.gwenski@gmail.com>
2019-07-10 16:15:49 +0300

    Create IncreasePricesCommand.cs

---
+++ BookShopSystem/BookShop/Core/Commands/IncreasePricesCommand.cs
@@ -0,0 +1,41 @@
+﻿using BookShop.Data;
+using BookShop_GoldenBooks_Added.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookShop.Core.Commands
+{
+    public class IncreasePricesCommand : ICommand
+    {
+        private readonly BookShopContext context;
+
+        public IncreasePricesCommand(BookShopContext context)
+        {
+            this.context = context;
+        }
+
+        public string Execute(string[] args)
+        {
+            IncreasePrices(context);
+            return "";
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            int releaseYear = 2010;
+
+            var books = context.Books
+                .Where(b => b.ReleaseDate.Year < releaseYear)
+                .ToList();
+
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
